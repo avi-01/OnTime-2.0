d6c3cc9b89e11be17948530433bc9e8a70c4c202
Avinsih kumar <avnishmay@gmail.com>
2019-07-04 22:38:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/avnis/ontime/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.avnis.ontime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+
+public class MainActivity extends AppCompatActivity {
+    CardView c1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        c1=(CardView)findViewById(R.id.card_view1);
+    }
+}
